81a8909eea2a59722317eaf3dd411a78ac99296e
Abdul Rafey <abdulrafey0824@gmail.com>
2024-08-14 16:55:06 +0500

    Added Suspense for suesearchparam

--- app/(pages)/(main)/subscription/my-plans/page.jsx
+++ app/(pages)/(main)/subscription/my-plans/page.jsx
@@ -13,7 +13,7 @@ import { useDispatch } from "react-redux";
 import StripeComponent from "../../../../components/stripe/StripeComponent";
 import { useSearchParams } from "next/navigation";
 
-const page = () => {
+const MyPlans = () => {
   const dispatch = useDispatch();
   const [isCheckedOut, setCheckedOut] = React.useState(false);
   const params = useSearchParams();
@@ -24,57 +24,62 @@ const page = () => {
   }, [dispatch]);
 
   return (
-    <Suspense>
-      <div className="p-4 pr-6 md:pr-4 bg-[#f9fafb] relative flex-1 flex flex-col space-y-4 w-screen md:w-full ">
-        {/* First container for Buttons */}
-        <div className="flex items-center justify-between space-x-4  w-full p-1">
-          <Link
-            href={"/subscription/plans"}
-            className="font-semibold text-lg cursor-pointer select-none"
-          >
-            Back
-          </Link>
-          {/* Add Payment Button */}
-          <GreenBtn
-            onClick={() => {
-              dispatch(setShowSideMenu({ value: true, mode: "add" }));
-            }}
-            title={"Add Payment Method"}
+    <div className="p-4 pr-6 md:pr-4 bg-[#f9fafb] relative flex-1 flex flex-col space-y-4 w-screen md:w-full ">
+      {/* First container for Buttons */}
+      <div className="flex items-center justify-between space-x-4  w-full p-1">
+        <Link
+          href={"/subscription/plans"}
+          className="font-semibold text-lg cursor-pointer select-none"
+        >
+          Back
+        </Link>
+        {/* Add Payment Button */}
+        <GreenBtn
+          onClick={() => {
+            dispatch(setShowSideMenu({ value: true, mode: "add" }));
+          }}
+          title={"Add Payment Method"}
+        />
+      </div>
+
+      {/* Main container */}
+      <div className="h-full bg-white border rounded-xl border-gray-300 p-6 gap-8 flex flex-col lg:flex-row justify-center items-center lg:justify-start lg:items-start text-center ">
+        {!isCheckedOut && (
+          <PlanBox
+            title={premium === "true" ? "Annual Plan" : "Monthly Plan"}
+            description={`Take Your Business to the Next Level with 21 days free trails of ${
+              premium === "true" ? "annual" : "monthly"
+            } plan`}
+            price={"$83"}
+            features={[
+              "Advanced Marketing Tools",
+              "Customizable Templates",
+              "Multi-user Access",
+              "Third-party Integrations",
+              "24/7 Priority Support",
+            ]}
+            btnGreen={true}
+            premium={premium === "true"}
+            myPlanBox={true}
           />
-        </div>
+        )}
 
-        {/* Main container */}
-        <div className="h-full bg-white border rounded-xl border-gray-300 p-6 gap-8 flex flex-col lg:flex-row justify-center items-center lg:justify-start lg:items-start text-center ">
-          {!isCheckedOut && (
-            <PlanBox
-              title={premium === "true" ? "Annual Plan" : "Monthly Plan"}
-              description={`Take Your Business to the Next Level with 21 days free trails of ${
-                premium === "true" ? "annual" : "monthly"
-              } plan`}
-              price={"$83"}
-              features={[
-                "Advanced Marketing Tools",
-                "Customizable Templates",
-                "Multi-user Access",
-                "Third-party Integrations",
-                "24/7 Priority Support",
-              ]}
-              btnGreen={true}
-              premium={premium === "true"}
-              myPlanBox={true}
-            />
-          )}
-
-          <StripeComponent premium={premium} />
-          {/* <PaymentCard
+        <StripeComponent premium={premium} />
+        {/* <PaymentCard
           isCheckedOut={isCheckedOut}
           setIsCheckedOut={setCheckedOut}
         /> */}
-          {/* <BankCard isCheckedOut={isCheckedOut} /> */}
-        </div>
+        {/* <BankCard isCheckedOut={isCheckedOut} /> */}
       </div>
-    </Suspense>
+    </div>
   );
 };
 
+const page = () => {
+  return (
+    <Suspense>
+      <MyPlans />
+    </Suspense>
+  );
+};
 export default page;
