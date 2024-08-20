8d05b07cd5a1e3a00209dbc233791bfd5ccbf68a
Abdul Rafey <abdulrafey0824@gmail.com>
2024-08-20 13:14:37 +0500

    Search function in variant input list

--- app/components/common/MultiInput.jsx
+++ app/components/common/MultiInput.jsx
@@ -11,7 +11,7 @@ const MultiInput = ({
   placeholder,
   name,
 }) => {
-  const [variantList, setVariantList] = React.useState([
+  const variantList = [
     "Front",
     "Rear",
     "Front Right",
@@ -26,8 +26,10 @@ const MultiInput = ({
     "Lower Front Left",
     "Lower Rear Right",
     "Lower Rear Left",
-  ]); // just for variant input
+  ]; // just for variant input
 
+  const [filteredVariantList, setFilteredVariantList] =
+    React.useState(variantList);
   const [variantMenu, setVariantMenu] = React.useState(false);
   const [variantInputVal, setVariantInputVal] = React.useState("");
 
@@ -36,8 +38,19 @@ const MultiInput = ({
   useEffect(() => {
     if (variantMenu && menuRef.current) {
       menuRef.current.scrollTop = 0; // Scroll to the top whenever the menu is opened
+      setVariantInputVal("");
+      setFilteredVariantList(variantList);
     }
   }, [variantMenu]);
+
+  const onVariantInputChange = (e) => {
+    setVariantInputVal(e.target.value);
+    setFilteredVariantList(
+      variantList.filter((item) =>
+        item.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  };
   return (
     <div className="w-full flex flex-wrap gap-2 gap-y-4 p-3 hover:border-gray-400 rounded-lg border border-[#D0D5DD]">
       {dataToMap?.map((item, index) => (
@@ -60,11 +73,12 @@ const MultiInput = ({
           onPressEnter(e.target[0].value);
           e.target[0].value = "";
         }}
-        onBlur={() =>
+        onBlur={() => {
           setTimeout(() => {
             setVariantMenu(false);
-          }, 200)
-        }
+            setVariantInputVal("");
+          }, 200);
+        }}
         tabIndex={0}
         className="w-full outline-none relative"
       >
@@ -77,26 +91,33 @@ const MultiInput = ({
               name={name}
               value={variantInputVal}
               onClick={() => setVariantMenu(!variantMenu)}
+              onChange={onVariantInputChange}
             />
             <div
               ref={menuRef}
               className={`${
                 variantMenu ? "block" : "hidden"
-              } bg-white overflow-auto no-scrollbar absolute top-[160%] w-full left-0  rounded-lg border border-black  flex flex-col justify-start max-h-40 pt-1 z-40`}
+              } bg-white overflow-auto absolute top-[160%] w-full left-0  rounded-lg border  shadow-md   flex flex-col justify-start max-h-40 pt-1 z-40`}
             >
-              {variantList.map((variant) => {
-                return (
-                  <p
-                    onClick={() => {
-                      onPressEnter(variant);
-                      setVariantMenu(!variantMenu);
-                    }}
-                    className="p-2 px-4 border-b-[1px]  cursor-pointer hover:bg-gray-100 font-medium"
-                  >
-                    {variant}
-                  </p>
-                );
-              })}
+              {filteredVariantList.length === 0 ? (
+                <p className="p-2 px-4 border-b-[1px]  cursor-pointer text-gray-500  font-medium">
+                  No Options
+                </p>
+              ) : (
+                filteredVariantList.map((variant) => {
+                  return (
+                    <p
+                      onClick={() => {
+                        onPressEnter(variant);
+                        setVariantMenu(!variantMenu);
+                      }}
+                      className="p-2 px-4 border-b-[1px]  cursor-pointer hover:bg-gray-100 font-medium"
+                    >
+                      {variant}
+                    </p>
+                  );
+                })
+              )}
             </div>
           </>
         ) : (
